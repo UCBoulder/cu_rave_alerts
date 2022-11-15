a8093ef51909513845f38b1895ed06ec87e61734
France <miles.france@gmail.com>
2022-11-15 23:35:16 +0000

    github: remove test var and move to releases env — adding phpstan test

--- .github/actions/cs/entrypoint.sh
+++ .github/actions/cs/entrypoint.sh
@@ -3,7 +3,7 @@
 # Fail on error
 set -e
 
-echo $repo
 export PATH="$PATH:/root/drupal/vendor/bin"
 echo "--------- CS O'Clock -------------"
 phpcs --standard=Drupal,DrupalPractice ./
+phpstan analyse -l 3 ./
